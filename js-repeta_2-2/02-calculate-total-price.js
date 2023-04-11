f516d9e3fc1cabc7ba1efdc4c1e93924e74613ab
Angey22 <babandval@gmail.com>
2023-04-11 19:32:32 +0300

    Fix video time in file '01-basics.js'

--- js-repeta_2-2/02-calculate-total-price.js
+++ js-repeta_2-2/02-calculate-total-price.js
@@ -1,8 +1,11 @@
+// ! Видео - 0:49:50...
+
 /*
  * Напиши функцию calculateTotalPrice(items)
  * которая принимает массив цен (чисел) и возвращает их сумму
  */
 
+//! Код скрипта подлежащего преобразованию в функцию:
 // const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
 // let total = 0;
 
@@ -12,6 +15,7 @@
 
 // console.log('Total: ', total);
 
+//! Код функции:
 const calculateTotalPrice = function (items) {
   console.log('items внутри функции: ', items);
 
@@ -24,8 +28,16 @@ const calculateTotalPrice = function (items) {
   return total;
 };
 
+// Вызов функции "calculateTotalPrice", через присвоение значения переменной "r1".
+console.log('Вызов функции - через присвоение значения переменной "r1":');
 const r1 = calculateTotalPrice([1, 2, 3]);
+console.log(`Общая сумма покупок ${r1}`); // => 6
+console.log('------------------------------------------------');
 
-console.log(`Общая сумма покупок ${r1}`); // 6
-// console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
-// console.log(calculateTotalPrice([100, 200, 300])); // 600
+// Вызов функции "calculateTotalPrice", через прямое обращение к ней "console.log()".
+console.log('Вызов функции - через прямое обращение к ней "console.log()":');
+
+console.log('"calculateTotalPrice([5, 10, 15, 20])" =>', calculateTotalPrice([5, 10, 15, 20])); // => 50
+console.log('------------------------------------------------');
+
+console.log('"calculateTotalPrice([100, 200, 300])" =>', calculateTotalPrice([100, 200, 300])); // => 600
