20fc169b75868184f6f252574973a494df3bf821
Angey22 <babandval@gmail.com>
2023-04-11 23:37:06 +0300

    Added code in file '08-arguments.js'

--- js-repeta_2-2/08-arguments.js
+++ js-repeta_2-2/08-arguments.js
@@ -1,46 +1,86 @@
+//! Видео - 1:43:40...2:11:28
+
 /*
  * Псевдомассив arguments и Array.from и ...
  */
+console.groupCollapsed('Псевдомассив arguments и Array.from'); //! Log group name
 
-// const fn = function () {
-//   console.log(arguments);
+//! Классический способ преобразования аргументов функции в массив с помощью метод "Array.from()".
+const fn = function () {
+  // Выводим в лог псевдомассив "arguments" функции "fn"
+  console.log('Псевдомассив - "arguments" =>', arguments);
 
-//   const args = Array.from(arguments);
+  // Преобразовываем в обычный массив псевод-массив "arguments" функции "fn". Используя для этого метод "Array.from()".
+  const args = Array.from(arguments);
+  console.log('Преобразованный в массив псевод-массив "args" =>', args);
+};
 
-//   console.log(args);
-// };
+fn();
+console.log('------------------------------------------------');
 
-// fn(1, 2, 3);
-// fn(1, 2, 3, 4, 5);
-// fn(1, 2, 3, 4, 5, 6, 7);
+fn(1, 2, 3);
+console.log('------------------------------------------------');
 
-// const fn = function (a, b, c, ...args) {
-//   console.log(`${a} ${b} ${c}`);
-//   console.log(args);
-// };
+fn(1, 2, 3, 4, 5);
+console.log('------------------------------------------------');
 
-// fn('hello', 1, 2, 3);
-// fn('aloha', 1, 2, 3, 4, 5);
-// fn('hi', 1, 2, 3, 4, 5, 6, 7);
+fn(1, 2, 3, 4, 5, 6, 7);
+console.log('------------------------------------------------');
+
+
+//! Современный способ преобразования аргументов функции в массив с помощью метода "...(rest)".
+const fn2 = function (...args2) {
+  console.log('Преобразованный в массив псевод-массив "args2" =>', args2);
+}
+
+fn2(1, 2, 3);
+console.log('------------------------------------------------');
+
+
+//! Комбинированный способ преобразования аргументов функции в массив.
+const fn3 = function (a, b, c, ...args) {
+  console.log(`${a} ${b} ${c}`);
+  console.log(args);
+};
+
+fn3('hello', 1, 2, 3);
+console.log('------------------------------------------------');
+
+fn3('aloha', 1, 2, 3, 4, 5);
+console.log('------------------------------------------------');
+
+fn3('hi', 1, 2, 3, 4, 5, 6, 7);
+console.log('------------------------------------------------');
+
+console.groupEnd(); //! Log group end
+console.log('-1----------------------------------------------');
+
 
 /*
  * Напиши функцию add для сложения произвольного количества аргументов (чисел)
  * - Операция ... (rest)
  */
+console.groupCollapsed('Функция "add" для сложения произвольного количества аргументов (чисел)'); //! Log group name
 
-// const add = function (...args) {
-//   console.log(args);
-//   let total = 0;
+const add = function (...args) {
+  console.log('Массив "args" внутри тела функции =>', args);
+  let total = 0;
 
-//   for (const arg of args) {
-//     total += arg;
-//   }
+  for (const arg of args) {
+    total += arg;
+  }
 
-//   return total;
-// };
+  return total;
+};
 
-// console.log(add(1, 2, 3));
-// console.log(add(1, 2, 4, 5, 6));
+console.log('Результат - "add(1, 2, 3)" =>', add(1, 2, 3));
+console.log('------------------------------------------------');
+
+console.log('Результат - "add(1, 2, 4, 5, 6)" =>', add(1, 2, 4, 5, 6));
+
+console.groupEnd(); //! Log group end
+console.log('-2----------------------------------------------');
+
 
 /*
  * Напиши функцию filterNumbers(array [, number1, ...]) которая:
@@ -49,6 +89,7 @@
  * - Функция должна вернуть новый массив, в котором будут только те аргументы, начиная со второго,
  *   для которых есть аналог в оригинальном массиве.
  */
+console.groupCollapsed('Функция - "filterNumbers(array [, number1, ...])"'); //! Log group name
 
 const filterNumbers = function (array, ...args) {
   console.log('array: ', array);
@@ -66,6 +107,14 @@ const filterNumbers = function (array, ...args) {
   return uniqueElements;
 };
 
-console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
-console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
-console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
+// Вызов функции "filterNumbers", через прямое обращение к ней "console.log()".
+console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // => [2, 3]
+console.log('------------------------------------------------');
+
+console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // => [30, 15]
+console.log('------------------------------------------------');
+
+console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // => [200]
+
+console.groupEnd(); //! Log group end
+console.log('-3----------------------------------------------');
