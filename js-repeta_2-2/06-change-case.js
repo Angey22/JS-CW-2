6795099c9f090417ca07b96fb614c4f34f9189a2
Angey22 <babandval@gmail.com>
2023-04-11 20:59:49 +0300

    Added notes in file '06-change-case.js'

--- js-repeta_2-2/06-change-case.js
+++ js-repeta_2-2/06-change-case.js
@@ -1,8 +1,11 @@
+//! Видео - 1:27:35...1:35:50
+
 /*
  * Напиши функцию changeCase(string) которая заменяет регистр
  * каждого символа в строке на противоположный.
  * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
  */
+// Смотри код скрипта из предыдущего занятия находящийся в файле "07-change-register.js"
 
 const changeCase = function (string) {
   const letters = string.split('');
@@ -19,6 +22,11 @@ const changeCase = function (string) {
   return invertedString;
 };
 
-console.log(changeCase('qweRTY')); // QWErty
-console.log(changeCase('mAnGo')); // MaNgO
-console.log(changeCase('AjAx')); // aJaX
+// Вызов функции "changeCase", через прямое обращение к ней "console.log()".
+console.log(changeCase('qweRTY')); // => QWErty
+console.log('------------------------------------------------');
+
+console.log(changeCase('mAnGo')); // => MaNgO
+console.log('------------------------------------------------');
+
+console.log(changeCase('AjAx')); // => aJaX
