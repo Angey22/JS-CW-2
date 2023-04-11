2530dec28d961be0f012fee3ef6f2f8a5fa3cb7c
Angey22 <babandval@gmail.com>
2023-04-11 20:36:04 +0300

    Added code in file '04-find-login.js'

--- js-repeta_2-2/04-find-login.js
+++ js-repeta_2-2/04-find-login.js
@@ -1,25 +1,26 @@
-//! Видео - 0:59:35...
+//! Видео - 0:59:35...1:20:20
 
 /*
  * Напиши функцию findLogin(allLogins, login) для поиска логина
  * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
  * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
  */
+// Смотри код скрипта из предыдущего занятия находящийся в файле "04-find-login.js"
 
-//! Код скрипта (из предыдущего занятия) подлежащего преобразованию в функцию:
+// Исходный массив - база данных
 const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
 
+//! 1-й вариант реализации функции
 // const findLogin = function (allLogins, loginToFind) {
 //   for (const login of allLogins) {
 //     if (login === loginToFind) {
 //       return `Пользователь ${loginToFind} найден.`;
 //     }
 //   }
-
 //   return `Пользователь ${loginToFind} не найден.`;
 // };
 
-//! Код функции:
+//! 2-й вариант реализации функции
 const findLogin = function (allLogins, loginToFind) {
   return allLogins.includes(loginToFind)
     ? `Пользователь ${loginToFind} найден.`
@@ -27,6 +28,12 @@ const findLogin = function (allLogins, loginToFind) {
 };
 
 console.log(findLogin(logins, 'avocod3r'));
+console.log('------------------------------------------------');
+
 console.log(findLogin(logins, 'k1widab3st'));
+console.log('------------------------------------------------');
+
 console.log(findLogin(logins, 'jam4l'));
+console.log('------------------------------------------------');
+
 console.log(findLogin(logins, 'poly1scute'));
